065abdccd44b6bf69c4326691283316135ca988a
KimSehoon <101192772+ki-met-hoon@users.noreply.github.com>
2024-10-24 16:36:12 +0900

    [Silver II] Title: N과 M (11), Time: 328 ms, Memory: 33952 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] nm = br.readLine().split(" ");
+        int n = Integer.parseInt(nm[0]);
+        int m = Integer.parseInt(nm[1]);
+
+        int[] numArr = new int[n];
+        String[] numStr = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            numArr[i] = Integer.parseInt(numStr[i]);
+        }
+        Arrays.sort(numArr);
+
+        int[] result = new int[m];
+        boolean[] visited = new boolean[n];
+        dfs(0, numArr, result, visited);
+
+        bw.write(String.valueOf(sb));
+        bw.flush();
+    }
+
+    private static void dfs(int depth, int[] numArr, int[] result, boolean[] visited) {
+        int before = 0;
+
+        if (depth == result.length) {
+            for (int i : result) {
+                sb.append(i).append(" ");
+            }
+
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < numArr.length; i++) {
+            if (before != numArr[i]) {
+                result[depth] = numArr[i];
+                before = numArr[i];
+                dfs(depth + 1, numArr, result, visited);
+            }
+        }
+    }
+}
