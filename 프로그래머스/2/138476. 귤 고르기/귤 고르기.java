53b6090639aa61c46eb3adcd8343eb528ad9d0e5
KimSehoon <101192772+ki-met-hoon@users.noreply.github.com>
2025-03-06 16:02:20 +0900

    [level 2] Title: 귤 고르기, Time: 111.38 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -3,41 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        ArrayList<Node> list = new ArrayList<>();
         
         for(int i : tangerine) {
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
         
-        for(int weight : map.keySet()) {
-            Node node = new Node(weight, map.get(weight));
-            list.add(node);
-        }
-        
-        list.sort((o1, o2) -> o1.count - o2.count);
+        ArrayList<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((o1, o2) -> map.get(o2) - map.get(o1));
             
         int ans = 0;
         
-        for(int i = list.size() - 1; i >= 0; i--) {
+        for(int key : list) {
             if(k <= 0) {
                 break;
             }
             
-            int nodeCount = list.get(i).count;
-            k -= nodeCount;
-            ans ++;
+            k -= map.get(key);
+            ans++;
         }
-        
-        return ans;
-    }
     
-    private class Node {
-        int weight;
-        int count;
-        
-        public Node(int weight, int count) {
-            this.weight = weight;
-            this.count = count;
-        }
+        return ans;
     }
 }
